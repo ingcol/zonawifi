7493525ab1d7add26ec1bc7f1ba47f1522397913
=dannergomez385@gmail.com <dannergomez385@gmail.com>
2020-09-22 10:15:00 -0500

    nombre

--- resources/views/layouts/app_login.blade.php
+++ resources/views/layouts/app_login.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Ayuda humanitaria</title>
+    <title>Zonas wifi kalu</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
